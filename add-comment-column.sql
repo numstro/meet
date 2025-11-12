6996ecf9d7484ed2e1d4b306490588506054eb8c
Kenny Chang <kennychang@numstro.com>
2025-11-11 21:48:36 -0800

    Add SQL migration to create comment column in poll_responses table

---
+++ add-comment-column.sql
@@ -0,0 +1,9 @@
+-- Add comment column to poll_responses table
+-- This allows participants to add comments to their votes
+
+ALTER TABLE poll_responses
+ADD COLUMN IF NOT EXISTS comment TEXT;
+
+-- Add a comment to document the column
+COMMENT ON COLUMN poll_responses.comment IS 'Optional comment from participant about their vote for this time slot';
+

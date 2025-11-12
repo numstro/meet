bf1a5a3cc54a1452549f2745667ede335d239ede
Kenny Chang <kennychang@numstro.com>
2025-11-11 22:23:21 -0800

    Add height: fit-content to table container and borderSpacing: 0 to prevent excess scroll space

--- app/poll/[id]/page.tsx
+++ app/poll/[id]/page.tsx
@@ -723,8 +723,8 @@ export default function PollPage() {
             </div>
             
             {/* Grid Table */}
-              <div className="overflow-x-auto border border-gray-200 rounded-lg" style={{ overscrollBehavior: 'contain' }}>
-                <table className="border-collapse m-0 w-full" style={{ tableLayout: 'auto' }}>
+              <div className="overflow-x-auto border border-gray-200 rounded-lg" style={{ overscrollBehavior: 'contain', height: 'fit-content' }}>
+                <table className="border-collapse m-0 w-full" style={{ tableLayout: 'auto', borderSpacing: 0 }}>
                   <thead>
                     <tr className="bg-gray-50">
                       <th className="text-left p-3 border-r border-gray-200 min-w-[150px] font-medium">
